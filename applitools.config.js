1a9f9e33734fda1bc6a1b73c55a7c5cff0e54db7
UsamaLT <osama.azhar@lantrotech.com>
2025-08-11 15:50:58 +0500

    adding all files to git repo

---
+++ applitools.config.js
@@ -0,0 +1,9 @@
+module.exports={
+    testConcurrency: 1,
+    apiKey: '3BJ0INmMkb107rzXuAcDFota104PY3yeKpcVqimRUN2vKvs110',
+    batchName: 'TAU Todo App',
+    browser: [
+        {width: 800, height:600, name:'chrome'},
+        {deviceName: 'Pixel 2',ScreenOrientation: "portrait"}
+    ]
+}
